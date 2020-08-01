41c7e76280058843c6077dcfbfb18d325bef5b24
MikeLogovi <40324123+MikeLogovi@users.noreply.github.com>
2020-08-01 17:14:01 +0100

    first commit

---
+++ src/main/java/com/dresseurdecode/blog/MyController.java
@@ -0,0 +1,19 @@
+package com.dresseurdecode.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyController {
+    @GetMapping("/")
+    public String home(@RequestParam String name, ModelMap modelMap){
+         modelMap.put("name",name);
+         return "hello";
+    }
+}
